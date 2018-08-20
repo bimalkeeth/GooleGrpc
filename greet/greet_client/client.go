874f3a70505e9d74e715aeafcb22b089d6771886
vtel.mobicom <bimalkeeth@gmail.com>
2018-08-21 05:47:08 +1000

    Grpc Exercise

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"GooleGrpc/greet/greetpb"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+)
 
+func main() {
+	fmt.Println("Hello Client")
+	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("Coud not connect", err)
+	}
+	defer conn.Close()
+	c := greetpb.NewGreetServiceClient(conn)
+
+	res, err := c.Greet(context.Background(), &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			First_Name: "Bimal",
+			Last_Name:  "Kaluarachchi",
+		},
+	})
+	if err != nil {
+		log.Fatal("Error occurred")
+	}
+	fmt.Println(res.GetResult())
 }
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -3,17 +3,37 @@ package main
 import (
 	"GooleGrpc/greet/greetpb"
 	"fmt"
-	"github.com/micro/util/go/lib/net"
 	"google.golang.org/grpc"
 	"log"
+	"net"
+
+	"context"
 )
 
 type server struct{}
 
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+
+	fmt.Println("Greet function invoked")
+
+	firstName := req.GetGreeting().GetFirst_Name()
+	lastName := req.GetGreeting().GetLast_Name()
+	result := "Hello" + firstName + " " + lastName
+
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
+const (
+	port = ":50051"
+)
+
 func main() {
 	fmt.Println("Hello world")
 
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatal(err)
 	}
